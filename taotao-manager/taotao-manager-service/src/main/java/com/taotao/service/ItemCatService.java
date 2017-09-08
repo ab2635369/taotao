5f1c9cdcb1e048c828745c5b9d68efc6852ce24d
zhongxj <379510281@qq.com>
2017-09-08 17:46:24 +0800

    catService,itemService,test

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemCatService.java
@@ -0,0 +1,11 @@
+package com.taotao.service;
+
+import java.util.List;
+
+import com.taotao.common.pojo.TreeNode;
+
+public interface ItemCatService {
+	
+	public List<TreeNode> getItemCatList(long parentId);
+
+}
---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,75 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
+import com.taotao.service.ItemService;
+
+/**
+ * 
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company:</p> 
+ * @author	钟欣江
+ * @date	2017年9月8日上午8:53:03
+ * @version 1.0
+ */
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		//添加查询条件
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if(list != null &&list.size()>0){
+			TbItem item = list.get(0);
+			return item;
+		}
+		
+		return null;
+	}
+
+	
+	/**
+	 * 商品列表查询
+	 * <p>Title: getItemList</p>
+	 * <p>Description: </p>
+	 * @param page
+	 * @param rows
+	 * @return
+	 * @see com.taotao.service.ItemService#getItemList(long, long)
+	 */
+	@Override
+	public EUDataGridResult getItemList(int page, int rows) {
+		//查询商品列表
+		TbItemExample example = new TbItemExample();
+		//分页处理
+		PageHelper.startPage(page,rows);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//创建一个返回值对象
+		EUDataGridResult result = new EUDataGridResult();
+		result.setRows(list);
+		//取记录总条数
+		PageInfo<TbItem> info = new PageInfo<>(list);
+		result.setTotal(info.getTotal());
+		return result;
+	}
+
+}
